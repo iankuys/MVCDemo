23ecc6bab5c98c0fa6e84358ad18ea01d94a34fe
iankuys <iankuyisien@gmail.com>
2020-09-17 16:38:52 +0800

    first commit

---
+++ BusinessLayer/EmployeeBusinessLayer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BusinessLayer
+{
+    public class EmployeeBusinessLayer
+    {
+
+        public void AddEmmployee(Employee employee)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["EmployeeContext"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spAddEmployeeMVC", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramName = new SqlParameter();
+                paramName.ParameterName = "@Name";
+                paramName.Value = employee.Name;
+                cmd.Parameters.Add(paramName);
+
+                SqlParameter paramGender = new SqlParameter();
+                paramGender.ParameterName = "@Gender";
+                paramGender.Value = employee.Gender;
+                cmd.Parameters.Add(paramGender);
+
+                SqlParameter paramCity = new SqlParameter();
+                paramCity.ParameterName = "@City";
+                paramCity.Value = employee.City;
+                cmd.Parameters.Add(paramCity);
+
+                SqlParameter paramDateOfBirth = new SqlParameter();
+                paramDateOfBirth.ParameterName = "@DateOfBirth";
+                paramDateOfBirth.Value = employee.DateOfBirth;
+                cmd.Parameters.Add(paramDateOfBirth);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿namespace MVCDemo.Controllers
+{
+    using MVCDemo.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using System.Web.Mvc;
+    using bs = BusinessLayer;
+
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            List<Employee> employee = employeeContext.Employees.Where(emp => emp.DepartmentId == id).ToList();
+            return View(employee);
+        }
+
+        public ActionResult Details(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee = new Employee();
+            employee = employeeContext.Employees.FirstOrDefault(emp => emp.EmployeeId == id);
+            return View(employee);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection formCollection)
+        {
+
+            bs.Employee employee = new bs.Employee();
+            //Retrieve form data using form collection
+            employee.Name = formCollection["Name"];
+            employee.Gender = formCollection["Gender"];
+            employee.City = formCollection["City"];
+            employee.DateOfBirth =
+                Convert.ToDateTime(formCollection["DateOfBirth"]);
+
+            bs.EmployeeBusinessLayer employeeBusinessLayer =
+                new bs.EmployeeBusinessLayer();
+
+            employeeBusinessLayer.AddEmmployee(employee);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
